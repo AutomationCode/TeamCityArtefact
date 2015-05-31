93e1d944e4a96c193879d32a1b30f4ce774e9b24
cityindexqa <david.costello@cityindex.com>
2015-05-31 21:10:51 +0100

    First commit of method returning year in string format

---
+++ ReturnTime.java
@@ -0,0 +1,17 @@
+import java.util.Date;
+
+/**
+ * Created by dmc on 31/05/2015.
+ */
+public class ReturnTime {
+
+    Date now;
+
+    public String GetYear()
+    {
+        String year;
+
+        year = "2016";
+        return year;
+    }
+}
